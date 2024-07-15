d1a1b5d4fd33dc825c6afcb5519e32641a2071fb
Daniel Bravo <daniel.brv@hotmail.cl>
2024-07-15 15:19:42 -0400

    add to sing in connection with strapi
    add to sing in connection with strapi

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -11,51 +11,70 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { loginUserAction } from "@/data/actions/auth-actions";
 
 function SignIn() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    try {
+      const prevState = {}; // Define el estado previo según sea necesario
+      await loginUserAction(prevState, formData);
+    } catch (error) {
+      console.error("Error registrando usuario:", error);
+    }
+  };
+
   return (
-    <Card className="mx-auto max-w-sm mt-6 mb-6">
-      <CardHeader>
-        <CardTitle className="text-2xl">Ingresar</CardTitle>
-        <CardDescription>
-          Ingrese su email debajo para ingresar a su cuenta
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Contraseña</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                ¿Olvidaste tu contraseña?
-              </Link>
+    <form onSubmit={handleSubmit}>
+      <Card className="mx-auto max-w-sm mt-6 mb-6">
+        <CardHeader>
+          <CardTitle className="text-2xl">Ingresar</CardTitle>
+          <CardDescription>
+            Ingrese su email debajo para ingresar a su cuenta
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="identifier">Email</Label>
+              <Input
+                id="identifier"
+                name="identifier"
+                type="text"
+                placeholder="m@example.com"
+                required
+              />
             </div>
-            <Input id="password" type="password" required />
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Contraseña</Label>
+                <Link
+                  href="#"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  ¿Olvidaste tu contraseña?
+                </Link>
+              </div>
+              <Input id="password" name="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Ingresar
+            </Button>
+            <Button variant="outline" className="w-full">
+              Ingresar con Google
+            </Button>
           </div>
-          <Button type="submit" className="w-full">
-            Ingresar
-          </Button>
-          <Button variant="outline" className="w-full">
-            Ingresar con Google
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          ¿No tienes cuenta?{" "}
-          <Link href="/getStarted" className="underline">
-            Registrate
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+          <div className="mt-4 text-center text-sm">
+            ¿No tienes cuenta?{" "}
+            <Link href="/getStarted" className="underline">
+              Registrate
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </form>
   );
 }
 
